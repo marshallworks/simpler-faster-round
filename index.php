eae110e9dfa722f63e4448581f17385f47c05ad5
Greg Marshall <greg@marshallworks.com>
2023-11-23 22:05:37 +0100

    vote interaction

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php include '_header.php'; ?>
-<h1>Search for Users</h1>
-<form class="smart" data-post="/user_search" data-target=".results">
-  <label for="name">Name: <input name="name" id="name" type="text"></label>
-  <button type="submit">Submit</button>
-</form>
-<div class="results"></div>
+<h1>Which Pokémon is Rounder?</h1>
+<div class="voter">
+  <div class="left">
+    <h2 id="name-left">Name</h2>
+    <img id="img-left" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png">
+    <button id="vote-left">Rounder</button>
+  </div>
+  <div class="or">or</div>
+  <div class="right">
+    <h2 id="name-right">Name</h2>
+    <img id="img-right" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png">
+    <button id="vote-right">Rounder</button>
+  </div>
+</div>
+<div style="display: none"><?php
+for ($i = 0; $i <= 1010; $i++) {
+  echo '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' . $i . '.png">';
+}
+?></div>
+<script type="application/json" id="pokemon-data"><?php include 'pokemon.json'; ?></script>
 <?php include '_footer.php'; ?>
 
--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-const smartElems = document.querySelectorAll('.smart');
-for (const elem of smartElems) {
-  switch (elem.tagName) {
-    case 'FORM': {
-      elem.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const inputs = elem.querySelectorAll('input');
-        const mess = [...inputs].map(i => {
-          return `${i.name}=${encodeURIComponent(i.value)}`;
-        }).join('&');
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', elem.dataset.post, true);
-        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xhr.onload = () => {
-          if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const target = document.querySelector(elem.dataset.target);
-            target.innerHTML = xhr.response;
-          }
-        };
-        xhr.send(mess);
-      });
-    } break;
-  }
+const STATE = {
+  data: JSON.parse(document.getElementById('pokemon-data').textContent),
+  left: {
+    id: 0,
+    name: document.getElementById('name-left'),
+    img: document.getElementById('img-left'),
+    vote: document.getElementById('vote-left'),
+  },
+  right: {
+    id: 0,
+    name: document.getElementById('name-right'),
+    img: document.getElementById('img-right'),
+    vote: document.getElementById('vote-right'),
+  },
+};
+
+function vote(state, id) {
+  console.log(id);
+  if (id === 0) console.log('init');
+  const leftId = Math.floor(Math.random() * state.data.length) + 1;
+  const rightId = Math.floor(Math.random() * state.data.length) + 1;
+
+  const left = state.data[leftId - 1];
+  const right = state.data[rightId - 1];
+
+  state.left.id = leftId;
+  state.right.id = rightId;
+  state.left.name.textContent = left[0];
+  state.right.name.textContent = right[0];
+  state.left.img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${leftId}.png`;
+  state.right.img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${rightId}.png`;
 }
+
+STATE.left.vote.addEventListener('click', () => {
+  vote(STATE, STATE.left.id);
+});
+STATE.right.vote.addEventListener('click', () => {
+  vote(STATE, STATE.right.id);
+});
+STATE.left.img.addEventListener('click', () => {
+  vote(STATE, STATE.left.id);
+});
+STATE.right.img.addEventListener('click', () => {
+  vote(STATE, STATE.right.id);
+});
+
+vote(STATE, 0);
+
