7886de003846b56c37ab3a7d276c72ff20f8fd28
Greg Marshall <greg@marshallworks.com>
2023-11-25 10:41:15 +0100

    minor img loading change

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Round But Simpler</title>
     <meta name="description" content="Simple version of faster-round.t3.gg.">
     <meta name="author" content="Greg Marshall">
+    <meta name="theme-color" content="#f3f4f6">
     <link rel="stylesheet" href="main.css">
   </head>
   <body>
@@ -13,13 +14,13 @@
     <div class="voter">
       <div class="left">
         <h2 id="name-left">Name</h2>
-        <img id="img-left" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png">
+        <img id="img-left" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png" alt="Image loading.">
         <button id="vote-left">Rounder</button>
       </div>
       <div class="or">or</div>
       <div class="right">
         <h2 id="name-right">Name</h2>
-        <img id="img-right" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png">
+        <img id="img-right" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png" alt="Image loading.">
         <button id="vote-right">Rounder</button>
       </div>
     </div>
@@ -29,7 +30,7 @@
     </div>
     <div class="hide"><?php
       for ($i = 0; $i <= 1010; $i++) {
-        echo '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' . $i . '.png">';
+        echo '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' . $i . '.png" fetchpriority="low">';
       }
     ?></div>
     <script type="application/json" id="pokemon-data"><?php include 'pokemon.json'; ?></script>
